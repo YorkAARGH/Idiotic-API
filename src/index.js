65e4e23fa1dfee9f01edd1d0cc19e1a4eeadeaf3
York <aarghgames@gmail.com>
2018-01-27 21:59:36 +0000

    Added endpoint functions
    Updated linter config file.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    Client: require('./lib/Client'),
-    version: require('../package.json').version
+  Client: require("./lib/Client"),
+  version: require("../package.json").version
 };
--- src/lib/Client.js
+++ src/lib/Client.js
@@ -1,30 +1,93 @@
-const snekfetch = require('snekfetch');
+const snekfetch = require("snekfetch");
 
 class Client {
 
-    constructor(token) {
-        if (!token) throw new Error(`Token must be specified`);
-        if (typeof token !== 'string') throw new SyntaxError(`Token must be a string`);
-        this.token = token;
+  constructor(token) {
+    if (!token) throw new Error("Token must be specified");
+    if (typeof token !== "string") throw new SyntaxError("Token must be a string");
+    this.token = token;
+    this.baseUrl = "http://api.anidiots.guide/api/";
+  }
 
-        this.baseUrl = 'http://api.anidiots.guide/api/';
-    }
+  // Text based
+  blame(text) {
+    return this._get("blame", { text });
+  }
 
-    achievement(avatar, text) {
-        return this._get('achievement', { avatar, text });
-    }
+  pls(text) {
+    return this._get("pls", { text });
+  }
 
-    _get(endpoint, query = {}) {
-        return new Promise((resolve, reject) => {
-            snekfetch.get(`${this.baseUrl}${endpoint}`)
-                .set('token', this.token)
-                .query(query)
-                .then(res => {
-                    if (res.status !== 200) return reject(res);
-                    return resolve(res.body);
-                }).catch(err => reject(err));
-        });
-    }
+  snapchat(text) {
+    return this._get("snapchat", { text });
+  }
+
+  // Image and Text
+  achievement(image, text) {
+    return this._get("achievement", { image, text });  
+  }  
+
+  theSearch(image, text) {
+    return this._get("thesearch", { image, text });  
+  }  
+
+  // Single Image
+  beautiful(image) {
+    return this._get("beautiful", { image });  
+  }
+
+  facepalm(image) {
+    return this._get("facepalm", { image });  
+  }
+
+  respect(image) {
+    return this._get("respect", { image });  
+  }
+
+  stepped(image) {
+    return this._get("stepped", { image });  
+  }
+
+  tattoo(image) {
+    return this._get("tattoo", { image });  
+  }
+
+  triggered(image) {
+    return this._get("triggered", { image });  
+  }
+
+  vaultBoy(image) {
+    return this._get("vault", { image });  
+  }
+  
+  wanted(image) {
+    return this._get("wanted", { image });  
+  }
+
+  // Double Image
+  batSlap(imageOne, imageTwo) {
+    return this._get("batslap", { imageOne, imageTwo });  
+  }
+
+  fanSlap(imageOne, imageTwo) {
+    return this._get("slap", { imageOne, imageTwo });  
+  }
+
+  crush(imageOne, imageTwo) {
+    return this._get("crush", { imageOne, imageTwo });    
+  }  
+
+  _get(endpoint, query = {}) {
+    return new Promise((resolve, reject) => {
+      snekfetch.get(`${this.baseUrl}${endpoint}`)
+        .set("token", this.token)
+        .query(query)
+        .then(res => {
+          if (res.status !== 200) return reject(res);
+          return resolve(res.body);
+        }).catch(err => reject(err));
+    });
+  }
 
 }
 
